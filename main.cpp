5152698019c70f890d2117eaf5af91d1ca8062a7
KI5UXW <ervinhfamily@gmail.com>
2026-01-06 13:54:25 -0600

    More work.

--- main.cpp
+++ main.cpp
@@ -15,47 +15,54 @@ std::vector<std::string> getActivationData(){
 
     std::cout<<"What's your callsign? (Ex. KI5UXW)"<<std::endl;
     std::string inputData;
-    std::cin >> inputData;
+    std::getline(std::cin, inputData);
     activData.push_back(inputData);
 
     std::cout<<"What's your name? (Ex. James Ervin)"<<std::endl;
-    std::string inputData;
-    std::cin >> inputData;
+    std::getline(std::cin, inputData);
     activData.push_back(inputData);
 
-    std::cout<<"What's your park name (Ex. Choke Canyon State Park)?"<<std::endl;
-    std::string inputData;
-    std::cin >> inputData;
+    std::cout<<"What's your park name? (Ex. Choke Canyon State Park)"<<std::endl;
+    std::getline(std::cin, inputData);
     activData.push_back(inputData);
 
-    std::cout<<"What's your park number (Ex. US-2997)?"<<std::endl;
-    std::string inputData;
-    std::cin >> inputData;
+    std::cout<<"What's your park number? (Ex. US-2997)"<<std::endl;
+    std::getline(std::cin, inputData);
     activData.push_back(inputData);
 
-    std::cout<<"What's your county (Ex. TX, McMullen)?"<<std::endl;
-    std::string inputData;
-    std::cin >> inputData;
+    std::cout<<"What's your county? (Ex. TX, McMullen)"<<std::endl;
+    std::getline(std::cin, inputData);
     activData.push_back(inputData);
 
-    std::cout<<"What's your transmit power in watts (Ex. 100)?"<<std::endl;
-    std::string inputData;
-    std::cin >> inputData;
+    std::cout<<"What's your state? (Ex. TX)"<<std::endl;
+    std::getline(std::cin, inputData);
+    activData.push_back(inputData);
+
+    std::cout<<"What's your country? (Ex. United States)"<<std::endl;
+    std::getline(std::cin, inputData);
+    activData.push_back(inputData);
+
+    std::cout<<"What's your transmit power in watts? (Ex. 100)"<<std::endl;
+    std::getline(std::cin, inputData);
     activData.push_back(inputData);
 
     std::cout<<"What's your frequency? (Ex. 28.333)"<<std::endl;
-    std::string inputData;
-    std::cin >> inputData;
+    std::getline(std::cin, inputData);
     activData.push_back(inputData);
 
     std::cout<<"What's your mode? (Ex. SSB)"<<std::endl;
-    std::string inputData;
-    std::cin >> inputData;
+    std::getline(std::cin, inputData);
     activData.push_back(inputData);
 
+    std::cout<<"What's the date in UTC? (Ex. 20260104)"<<std::endl;
+    std::getline(std::cin, inputData);
+    activData.push_back(inputData);
+
+    std::cout<<activData.size()<<std::endl;
+
     for (long unsigned int n = 0; n < activData.size(); n++){
-        std::cout<< activData[n]<<" ";
+        std::cout<< activData.at(n)<<" ";
     }
     return activData;
-    //Testing here.
+
 }
